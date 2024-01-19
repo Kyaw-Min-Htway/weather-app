c30fc4b033ac24dede4e10877bd75fcc5e17aa66
KyawMinHtway066813 <batman066813@gmail.com>
2024-01-19 20:13:44 +0630

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7067c02a0amshb171459e5ef5f94p153339jsnd5a4fc4b65c7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
